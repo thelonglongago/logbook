36e3494cb9754f78dd6bbb2606eeadfe5bb36bd2
thelonglongago <eric.cardoso1@gmail.com>
2015-02-05 23:04:15 -0500

    Fourth Commit
    Added rudimentary nav bar. Root to index

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/search'
 
+  root 'static_pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
